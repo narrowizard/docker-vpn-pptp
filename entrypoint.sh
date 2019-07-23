bc4c847cca28e7c04cbb3fe2350284583a4028d7
narro <501xs05@163.com>
2019-07-23 10:41:51 +0800

    iptables config

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ $client    $server      $password    $acceptable_local_ip_addresses
 
 
 # configure firewall
-iptables -t nat -A POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
 iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -P FORWARD ACCEPT
 
 exec "$@"
